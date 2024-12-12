60d2835fd49e9bc2b1c763dd325f48fdb81059b6
Rustam Mamedov <knife2603@gmail.com>
2024-12-12 07:23:23 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import * as Sentry from '@sentry/browser';
+
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+
+  dsn: 'YOUR_SENTRY_DSN',
+
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+
+});
+
+
+
